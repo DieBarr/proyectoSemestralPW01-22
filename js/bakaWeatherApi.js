9d6febf2315ab3aeb627073a6be171d20ccb7a54
Diego Barros <diegobarrosaraya@outlook.com>
2022-05-02 11:25:25 -0400

    cambio nombre por redundancia, afinada vista admin

--- js/bakaWeatherApi.js
+++ js/bakaWeatherApi.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     "https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/japan?unitGroup=metric&include=current&key=R4RLJV5TR6V7E44NPWVZMGVZ5&lang=es&contentType=json",
     function (data) {
       $("#clima").append(
-        "<div class='col-12'><img src='resources/bakaNeko2.svg' width='15%'><h1>BakaWeather</h1>El clima donde vive tu waifu (´,,•ω•,,)♡ ahorita es: <br><h1>  " +
+        "<div class='col-12'><img src='../resources/bakaNeko2.svg' width='15%'><h1>BakaWeather</h1>El clima donde vive tu waifu (´,,•ω•,,)♡ ahorita es: <br><h1>  " +
           /*Propuesta, anadir condicional de temperatura, Ej: si hacen menos de 15C anadir al final "Enviale un chalequito pa que no pase frio („ಡωಡ„)"*/
           data.currentConditions.temp +
           "°C</h1>" +
